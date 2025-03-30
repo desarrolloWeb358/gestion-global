ab74a61388bacf147d4b727fa79acc0eb4828c0b
Juan Pablo Duque <desarrolloweb358@gmail.com>
2025-03-30 16:58:02 -0500

    configuracion twilio

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -10,8 +10,21 @@
 import {onRequest} from "firebase-functions/v2/https";
 import * as logger from "firebase-functions/logger";
 
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
+import * as functions from 'firebase-functions';
+import { sendSMS } from './twilio/sendSMS';
+
+export const sendTestSMS = functions.https.onRequest(async (req, res) => {
+  const to = req.query.to as string;
+  const body = req.query.body as string || 'Hola desde Firebase + Twilio!';
+
+  try {
+    const sid = await sendSMS(to, body);
+    res.status(200).send(`Mensaje enviado: ${sid}`);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error enviando el mensaje.');
+  }
+});
 
 export const helloWorld = onRequest((req, res) => {
     logger.info("Hello logs!", { structuredData: true });
