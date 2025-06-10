e3bf51a571bded1585ca44dde91dad7e262cacb3
SuperCerebros <aprendizajerapido@gmail.com>
2025-06-10 17:36:06 -0500

    iconos y menu

--- frontend/src/shared/components/layout/AppLayout.tsx
+++ frontend/src/shared/components/layout/AppLayout.tsx
@@ -4,6 +4,7 @@ import { Outlet } from "react-router";
 import Backdrop from "./Backdrop";
 import AppSidebar from "./AppSidebar";
 import React from "react";
+import SidebarToggleButton from "./SidebarToggleButton";
 
 const LayoutContent: React.FC = () => {
   const { isExpanded, isHovered, isMobileOpen } = useSidebar();
@@ -15,11 +16,14 @@ const LayoutContent: React.FC = () => {
         <Backdrop />
       </div>
       <div
-        className={`flex-1 transition-all duration-300 ease-in-out ${
-          isExpanded || isHovered ? "lg:ml-[290px]" : "lg:ml-[90px]"
-        } ${isMobileOpen ? "ml-0" : ""}`}
+              className={`flex-1 transition-all duration-300 ease-in-out relative
+          ${isExpanded || isHovered ? "lg:ml-[290px]" : "lg:ml-[90px]"}
+          ${isMobileOpen ? "ml-0" : ""}
+        `}
       >
+
         {/* <AppHeader /> */}
+        <SidebarToggleButton /> {/* ✅ BOTÓN flotante */}
         <div className="p-4 mx-auto max-w-(--breakpoint-2xl) md:p-6">
           <Outlet />
         </div>
