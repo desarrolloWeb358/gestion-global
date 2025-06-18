b25936a65ac47a5ef1f4a41c05da2fa3f36803ad
SuperCerebros <aprendizajerapido@gmail.com>
2025-06-18 13:35:34 -0500

    Removido el código fuente de funciones del repositorio por seguridad

--- functions/src/twilio/client.ts
+++
@@ -1,8 +0,0 @@
-import { Twilio } from 'twilio';
-import * as dotenv from 'dotenv';
-dotenv.config();
-
-export const twilioClient = new Twilio(
-  process.env.TWILIO_ACCOUNT_SID!,
-  process.env.TWILIO_AUTH_TOKEN!
-);
