4e3b1fe5a118697b8ae2fa2e7aaf10f6083bff7a
Juan Pablo Duque <juanpabloduque@gmail.com>
2026-04-08 10:49:03 -0500

    justificacion y ancho de renglones en edicion de texto

--- frontend/src/shared/components/RichTextEditor.tsx
+++ frontend/src/shared/components/RichTextEditor.tsx
@@ -167,6 +167,16 @@ export default function RichTextEditor({
           >
             →
           </ToolbarButton>
+
+          <ToolbarButton
+            title="Justificar"
+            active={editor?.isActive({ textAlign: "justify" })}
+            onClick={() => editor?.chain().focus().setTextAlign("justify").run()}
+          >
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" width="14" height="14" fill="currentColor">
+              <path d="M0 1.5A.5.5 0 0 1 .5 1h15a.5.5 0 0 1 0 1H.5a.5.5 0 0 1-.5-.5zm0 3A.5.5 0 0 1 .5 4h15a.5.5 0 0 1 0 1H.5a.5.5 0 0 1-.5-.5zm0 3A.5.5 0 0 1 .5 7h15a.5.5 0 0 1 0 1H.5a.5.5 0 0 1-.5-.5zm0 3a.5.5 0 0 1 .5-.5h15a.5.5 0 0 1 0 1H.5a.5.5 0 0 1-.5-.5zm0 3a.5.5 0 0 1 .5-.5h15a.5.5 0 0 1 0 1H.5a.5.5 0 0 1-.5-.5z"/>
+            </svg>
+          </ToolbarButton>
         </div>
       )}
 
